04d7df64a07a5e097271eea8c07e812486e21038
Ioan Solovastru <nelusolovastru@gmail.com>
2022-06-12 12:40:56 +0300

    git ignore added

---
+++ nextjs-frontend-lottery/README.md
@@ -0,0 +1,7 @@
+## Installation
+
+Install nextjs using yarn:
+
+```bash
+yarn create next-app .
+```
---
+++ nextjs-frontend-lottery/pages/index.js
@@ -0,0 +1,16 @@
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Smart Contract Raffle</title>
+        <meta name="description" content="Smart contract raffle" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      Hi
+    </div>
+  );
+}
